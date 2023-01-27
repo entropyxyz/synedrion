5513d02aca0fa0b88912d45afc76da177ce7bb30
Bogdan Opanchuk <bogdan@opanchuk.net>
2023-01-26 20:49:28 -0800

    Use the RandomPrimeWithRng trait

--- src/paillier/uint.rs
+++ src/paillier/uint.rs
@@ -1,19 +1,19 @@
-use crypto_bigint::{Integer, RandomMod, Zero, U128, U64};
-use crypto_primes::safe_prime;
+use rand_core::OsRng;
 
-pub trait Uint: Zero + Integer + RandomMod {
+use crypto_bigint::{Integer, RandomMod, Zero, U128, U64};
+use crypto_primes::RandomPrimeWithRng;
+
+pub trait Uint: Zero + Integer + RandomMod + RandomPrimeWithRng {
     fn sub(&self, rhs: &Self) -> Self;
-    fn safe_prime() -> Self;
     fn mul_wide(&self, rhs: &Self) -> (Self, Self);
+    fn safe_prime() -> Self {
+        Self::safe_prime_with_rng(&mut OsRng, Self::BITS)
+    }
 }
 
 impl Uint for U64 {
     fn sub(&self, rhs: &Self) -> Self {
         self.wrapping_sub(rhs)
-    }
-
-    fn safe_prime() -> Self {
-        safe_prime(Self::BITS)
     }
 
     fn mul_wide(&self, rhs: &Self) -> (Self, Self) {
@@ -24,10 +24,6 @@ impl Uint for U64 {
 impl Uint for U128 {
     fn sub(&self, rhs: &Self) -> Self {
         self.wrapping_sub(rhs)
-    }
-
-    fn safe_prime() -> Self {
-        safe_prime(Self::BITS)
     }
 
     fn mul_wide(&self, rhs: &Self) -> (Self, Self) {
