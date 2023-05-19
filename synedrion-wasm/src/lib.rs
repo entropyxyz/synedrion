fe6ce75f8d873b2535197f3fd1c45c36c55bb2c9
Bogdan Opanchuk <bogdan@opanchuk.net>
2023-05-19 16:24:18 -0700

    Return typed values in WASM make_key_shares(), add WASM tests

--- synedrion-wasm/src/lib.rs
+++ synedrion-wasm/src/lib.rs
@@ -5,51 +5,60 @@ use bincode::{
     },
     DefaultOptions, Options,
 };
-use js_sys::{Error, Uint8Array};
+use js_sys::Error;
 use rand_core::OsRng;
+use wasm_bindgen::{prelude::wasm_bindgen, JsCast, JsValue};
+use wasm_bindgen_derive::TryFromJsValue;
+
 use synedrion::TestSchemeParams;
-use wasm_bindgen::{
-    prelude::{wasm_bindgen, JsValue},
-    JsCast,
-};
+
+extern crate alloc;
 
 /// Max message length allowed to be (de)serialized
 const MAX_MSG_LEN: u64 = 1000 * 1000; // 1 MB
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(typescript_type = "Uint8Array[]")]
-    pub type ShareArray;
+    #[wasm_bindgen(typescript_type = "KeyShare[]")]
+    pub type KeyShareArray;
 }
 
-/// Create a set of key shares
-#[wasm_bindgen(js_name = makeKeyShares)]
-pub fn make_key_shares(num_parties: usize) -> Result<ShareArray, Error> {
-    let shares = synedrion::make_key_shares::<TestSchemeParams>(&mut OsRng, num_parties);
+/// Synedrion key share.
+#[derive(TryFromJsValue)]
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct KeyShare(synedrion::KeyShare<TestSchemeParams>);
 
-    let bincode = bincoder();
-    let mut shares_vec = Vec::<JsValue>::with_capacity(num_parties);
-
-    for share in shares.iter() {
-        let share_serialized = bincode
-            .serialize(&share)
-            .map_err(|err| Error::new(&format!("{}", err)))?;
-
-        let share_js: JsValue = Uint8Array::from(share_serialized.as_ref()).into();
-        shares_vec.push(share_js)
+#[wasm_bindgen]
+impl KeyShare {
+    /// Serializes the key share to bytes using standard Entropy format.
+    #[wasm_bindgen(js_name = toBytes)]
+    pub fn to_bytes(&self) -> Result<Vec<u8>, Error> {
+        // TODO: can we ensure consistency here?
+        // Should `entropy-core` expose the serialization function?
+        bincoder()
+            .serialize(&self.0)
+            .map_err(|err| Error::new(&format!("{}", err)))
     }
-
-    Ok(shares_vec
-        .into_iter()
-        .collect::<js_sys::Array>()
-        .unchecked_into::<ShareArray>())
 }
 
-/// Prepare a `bincode` serde backend with our preferred config
+/// Creates a set of key shares.
+#[wasm_bindgen(js_name = makeKeyShares)]
+pub fn make_key_shares(num_parties: usize) -> KeyShareArray {
+    let shares = synedrion::make_key_shares::<TestSchemeParams>(&mut OsRng, num_parties);
+    shares
+        .into_vec()
+        .into_iter()
+        .map(KeyShare)
+        .map(JsValue::from)
+        .collect::<js_sys::Array>()
+        .unchecked_into::<KeyShareArray>()
+}
+
+/// Prepares a `bincode` serde backend with our preferred config
 /// This is copied from `entropy-core/crypto/kvdb/src/kv_manager/helpers.rs`
 /// In the hope that it gives us shares which are encoded just the same as
 /// the are in the key value store there.
-// TODO: can we ensure consistency here? Should `entropy-core` expose the serialization function?
 #[allow(clippy::type_complexity)]
 fn bincoder() -> WithOtherTrailing<
     WithOtherIntEncoding<
---
+++ synedrion-wasm/tests/wasm.rs
@@ -0,0 +1,27 @@
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_test::wasm_bindgen_test;
+
+use synedrion_wasm::{make_key_shares, KeyShare};
+
+fn try_from_js_array<T>(val: impl Into<JsValue>) -> Vec<T>
+where
+    for<'a> T: TryFrom<&'a JsValue>,
+    for<'a> <T as TryFrom<&'a JsValue>>::Error: core::fmt::Debug,
+{
+    let js_array: js_sys::Array = val.into().dyn_into().unwrap();
+    js_array
+        .iter()
+        .map(|js| T::try_from(&js).unwrap())
+        .collect::<Vec<_>>()
+}
+
+#[wasm_bindgen_test]
+fn test_make_key_shares() {
+    let shares_js = make_key_shares(3);
+    let shares = try_from_js_array::<KeyShare>(shares_js);
+    let _shares_serialized = shares
+        .iter()
+        .map(|share| share.to_bytes())
+        .collect::<Vec<_>>();
+    // TODO: serialize synedrion::KeyShare and compare sizes?
+}
