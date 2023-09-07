e237b21a93469b9bd8afa873d536cda62a648397
Bogdan Opanchuk <bogdan@opanchuk.net>
2023-09-06 23:12:52 -0700

    Rearrange tests in the benchmarks and use 2 parties instead of 3

--- synedrion/benches/bench.rs
+++ synedrion/benches/bench.rs
@@ -7,24 +7,23 @@ fn bench_happy_paths(c: &mut Criterion) {
     let mut group = c.benchmark_group("happy path");
 
     type Params = TestParams;
+    let key_shares = KeyShare::new_centralized(&mut OsRng, 2, None);
 
-    group.bench_function("KeyGen, 3 parties", |b| {
-        b.iter(|| benches::keygen::<Params>(&mut OsRng, 3))
+    group.bench_function("KeyGen, 2 parties", |b| {
+        b.iter(|| benches::keygen::<Params>(&mut OsRng, 2))
     });
 
-    group.bench_function("KeyRefresh, 3 parties", |b| {
-        b.iter(|| benches::key_refresh::<Params>(&mut OsRng, 3))
+    group.bench_function("Signing, 2 parties", |b| {
+        b.iter(|| benches::signing::<Params>(&mut OsRng, &key_shares))
     });
-
-    let key_shares = KeyShare::new_centralized(&mut OsRng, 3, None);
 
     group.sample_size(10);
-    group.bench_function("Presigning, 3 parties", |b| {
+    group.bench_function("Presigning, 2 parties", |b| {
         b.iter(|| benches::presigning::<Params>(&mut OsRng, &key_shares))
     });
 
-    group.bench_function("Signing, 3 parties", |b| {
-        b.iter(|| benches::signing::<Params>(&mut OsRng, &key_shares))
+    group.bench_function("KeyRefresh, 2 parties", |b| {
+        b.iter(|| benches::key_refresh::<Params>(&mut OsRng, 2))
     });
 
     group.finish()
