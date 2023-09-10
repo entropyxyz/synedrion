93990703c48c2ae2ab8521e9c8d6313658624753
Bogdan Opanchuk <bogdan@opanchuk.net>
2023-09-10 00:08:32 -0700

    Add docs for PaillierParams fields

--- synedrion/src/paillier/params.rs
+++ synedrion/src/paillier/params.rs
@@ -6,18 +6,32 @@ use crate::uint::{
 };
 
 pub trait PaillierParams: PartialEq + Eq + Clone + core::fmt::Debug + Send {
+    /// The size of one of the pair of RSA primes.
     const PRIME_BITS: usize;
+    /// The size of the RSA modulus (a product of two primes).
     const MODULUS_BITS: usize = Self::PRIME_BITS * 2;
+    /// An integer that fits a single RSA prime.
     type SingleUint: UintLike + HasWide<Wide = Self::DoubleUint>;
+    /// A modulo-residue counterpart of `SingleUint`.
     type SingleUintMod: UintModLike<RawUint = Self::SingleUint>;
+    /// An integer that fits the RSA modulus.
     type DoubleUint: UintLike
         + FromScalar
         + HasWide<Wide = Self::QuadUint>
         + Serialize
         + for<'de> Deserialize<'de>;
+    /// A modulo-residue counterpart of `DoubleUint`.
     type DoubleUintMod: UintModLike<RawUint = Self::DoubleUint>;
+    /// An integer that fits the squared RSA modulus.
+    /// Used for Paillier ciphertexts.
     type QuadUint: UintLike + Serialize + for<'de> Deserialize<'de> + HasWide<Wide = Self::OctoUint>;
+    /// A modulo-residue counterpart of `QuadUint`.
     type QuadUintMod: UintModLike<RawUint = Self::QuadUint>;
+    /// An integer that fits the squared RSA modulus times a small factor.
+    /// Used in some ZK proofs.
+    // Technically, it doesn't have to be that large, but the time spent multiplying these
+    // is negligible, and when it is used as an exponent, it is bounded anyway.
+    // So it is easier to keep it as a double of `QuadUint`.
     type OctoUint: UintLike + Serialize + for<'de> Deserialize<'de>;
 }
 
@@ -25,7 +39,7 @@ pub trait PaillierParams: PartialEq + Eq + Clone + core::fmt::Debug + Send {
 pub struct PaillierTest;
 
 impl PaillierParams for PaillierTest {
-    // We need 257-bit primes because we need DoubleUint to accommodate all the possible
+    // We need 257-bit primes because we need MODULUS_BITS to accommodate all the possible
     // values of curve scalar squared, which is 512 bits.
     const PRIME_BITS: usize = 257;
     type SingleUint = U512;
@@ -41,8 +55,6 @@ impl PaillierParams for PaillierTest {
 pub struct PaillierProduction;
 
 impl PaillierParams for PaillierProduction {
-    // We need 257-bit primes because we need DoubleUint to accommodate all the possible
-    // values of curve scalar squared, which is 512 bits.
     const PRIME_BITS: usize = 1024;
     type SingleUint = U1024;
     type SingleUintMod = U1024Mod;
