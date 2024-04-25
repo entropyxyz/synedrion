f899e218e02df44ad00077194550ed396846836b
Bogdan Opanchuk <bogdan@opanchuk.net>
2024-04-25 14:18:32 -0700

    Rename `states.rs` to `session.rs`

--- synedrion/src/sessions.rs
+++ synedrion/src/sessions.rs
@@ -3,13 +3,13 @@
 mod combined_message;
 mod echo;
 mod error;
+mod session;
 mod signed_message;
-mod states;
 mod type_erased;
 
 pub use combined_message::CombinedMessage;
 pub use echo::EchoError;
 pub use error::{Error, LocalError, ProvableError, RemoteError, RemoteErrorEnum};
-pub use states::{
+pub use session::{
     Artifact, FinalizeOutcome, PreprocessedMessage, ProcessedMessage, RoundAccumulator, Session,
 };
