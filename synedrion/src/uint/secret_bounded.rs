1f078e72dc038bb462ffd08a550f6cb16219c0ec
Bogdan Opanchuk <bogdan@opanchuk.net>
2024-12-09 23:40:34 -0800

    Use Secret in SecretBounded

--- synedrion/src/uint/secret_bounded.rs
+++ synedrion/src/uint/secret_bounded.rs
@@ -1,15 +1,15 @@
 use crypto_bigint::{Bounded, Integer, NonZero};
-use zeroize::{Zeroize, ZeroizeOnDrop};
+use zeroize::Zeroize;
 
 use super::{HasWide, Signed};
 use crate::tools::Secret;
 
 /// A bounded integer with sensitive data.
-#[derive(Debug, Clone, PartialEq, Eq, Default, ZeroizeOnDrop)]
+#[derive(Debug, Clone)]
 pub(crate) struct SecretBounded<T: Zeroize> {
     /// bound on the bit size of the value
     bound: u32,
-    value: T,
+    value: Secret<T>,
 }
 
 impl<T> SecretBounded<T>
@@ -29,30 +29,37 @@ where
         if bound > T::BITS || value.bits() > bound {
             return None;
         }
-        Some(Self { value, bound })
+        Some(Self {
+            value: Secret::init_with(|| value),
+            bound,
+        })
     }
 
     pub fn add_mod(&self, rhs: &Self, modulus: &Secret<NonZero<T>>) -> Self {
         // Note: assuming that the bit size of the modulus is not secret
         // (although the modulus itself might be)
         Self {
-            value: self.value.add_mod(&rhs.value, modulus.expose_secret()),
+            value: Secret::init_with(|| {
+                self.value
+                    .expose_secret()
+                    .add_mod(rhs.value.expose_secret(), modulus.expose_secret())
+            }),
             bound: modulus.expose_secret().bits_vartime(),
         }
     }
 
     pub fn to_signed(&self) -> Option<Secret<Signed<T>>> {
-        Secret::maybe_init_with(|| Signed::new_positive(self.value.clone(), self.bound))
+        Secret::maybe_init_with(|| Signed::new_positive(self.value.expose_secret().clone(), self.bound))
     }
 
     pub fn expose_secret(&self) -> &T {
-        &self.value
+        self.value.expose_secret()
     }
 }
 
 impl<T> SecretBounded<T>
 where
-    T: Zeroize + HasWide,
+    T: Zeroize + Clone + HasWide,
     T::Wide: Zeroize,
 {
     pub fn to_wide(&self) -> SecretBounded<T::Wide> {
