a299252f6f5b9da3bca4f68ccc17ffc4d86fe9be
Bogdan Opanchuk <bogdan@opanchuk.net>
2024-12-21 00:15:05 -0800

    Update mod

--- synedrion/src/cggmp21/sigma/mod_.rs
+++ synedrion/src/cggmp21/sigma/mod_.rs
@@ -1,8 +1,8 @@
-//! Proof of Paillier-Blum modulus ($\Pi^{mod}$, Fig. 16)
+//! Proof of Paillier-Blum modulus ($\Pi^{mod}$, Fig. 12)
 
 use alloc::vec::Vec;
 
-use crypto_bigint::{modular::Retrieve, Square};
+use crypto_bigint::{modular::Retrieve, Gcd, Integer, Invert, Square};
 use crypto_primes::RandomPrimeWithRng;
 use digest::XofReader;
 use rand::SeedableRng;
@@ -13,7 +13,7 @@ use serde::{Deserialize, Serialize};
 use super::super::SchemeParams;
 use crate::{
     paillier::{PaillierParams, PublicKeyPaillier, SecretKeyPaillier},
-    tools::hashing::{uint_from_xof_modulo, Chain, Hashable, XofHasher},
+    tools::hashing::{Chain, Hashable, XofHasher},
     uint::{Exponentiable, ToMontgomery},
 };
 
@@ -38,10 +38,8 @@ impl<P: SchemeParams> ModChallenge<P> {
             .chain(commitment)
             .chain(aux)
             .finalize_to_reader();
-
-        let modulus = pk.modulus_nonzero();
         let ys = (0..P::SECURITY_PARAMETER)
-            .map(|_| uint_from_xof_modulo(&mut reader, &modulus))
+            .map(|_| pk.invertible_residue_from_xof_reader(&mut reader))
             .collect();
         Self(ys)
     }
@@ -81,7 +79,7 @@ impl<P: SchemeParams> ModProof<P> {
         let commitment = ModCommitment::<P>::random(rng, sk);
         let challenge = ModChallenge::<P>::new(pk, &commitment, aux);
 
-        let (omega_mod_p, omega_mod_q) = sk.rns_split(&commitment.0);
+        let (w_mod_p, w_mod_q) = sk.rns_split(&commitment.0);
 
         let proof = challenge
             .0
@@ -97,8 +95,8 @@ impl<P: SchemeParams> ModProof<P> {
                         y_mod_q = -y_mod_q;
                     }
                     if *b {
-                        y_mod_p *= omega_mod_p.clone();
-                        y_mod_q *= omega_mod_q.clone();
+                        y_mod_p *= w_mod_p.clone();
+                        y_mod_q *= w_mod_q.clone();
                     }
 
                     if let Some((p, q)) = sk.rns_sqrt(&(y_mod_p, y_mod_q)) {
@@ -155,11 +153,20 @@ impl<P: SchemeParams> ModProof<P> {
             return false;
         }
 
+        if pk.modulus().gcd(&self.commitment.0) != <P::Paillier as PaillierParams>::Uint::one() {
+            return false;
+        }
+
         let monty_params = pk.monty_params_mod_n();
-        let omega_mod = self.commitment.0.to_montgomery(monty_params);
+        let w_mod = self.commitment.0.to_montgomery(monty_params);
         for (elem, y) in self.proof.iter().zip(self.challenge.0.iter()) {
             let z_m = elem.z.to_montgomery(monty_params);
             let mut y_m = y.to_montgomery(monty_params);
+
+            if y_m.invert().is_none().into() {
+                return false;
+            }
+
             let pk_modulus = pk.modulus_signed();
             if z_m.pow(&pk_modulus) != y_m {
                 return false;
@@ -169,7 +176,7 @@ impl<P: SchemeParams> ModProof<P> {
                 y_m = -y_m;
             }
             if elem.b {
-                y_m *= omega_mod;
+                y_m *= w_mod;
             }
             let x = elem.x.to_montgomery(monty_params);
             let x_4 = x.square().square();
