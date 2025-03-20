a601e6803255e74f0289a61fca9606ed4788bbc0
Bogdan Opanchuk <bogdan@opanchuk.net>
2025-03-20 12:13:11 -0700

    Remove `CURVE_ORDER` and `CURVE_ORDER_WIDE` from `SchemeParams`

--- src/params/conversion.rs
+++ src/params/conversion.rs
@@ -1,5 +1,5 @@
-use crypto_bigint::{BitOps, Zero};
-use elliptic_curve::{CurveArithmetic, PrimeField};
+use crypto_bigint::{BitOps, NonZero, Zero};
+use elliptic_curve::{bigint::Encoding, Curve, CurveArithmetic, PrimeField};
 use secrecy::{ExposeSecret, ExposeSecretMut, SecretBox};
 
 use super::traits::SchemeParams;
@@ -9,6 +9,27 @@ use crate::{
     tools::Secret,
     uint::{BoxedEncoding, Extendable, PublicSigned, SecretSigned},
 };
+
+fn curve_order<P: SchemeParams>() -> <P::Paillier as PaillierParams>::Uint {
+    // The current version of `elliptic_curve` uses `crypto-bigint` 0.5,
+    // and our Paillier code uses 0.6. This function converts the integer between the two.
+    let order_repr = P::Curve::ORDER.to_be_bytes();
+    let mut uint_repr = <P::Paillier as PaillierParams>::Uint::zero().to_be_bytes();
+    let uint_repr_len = uint_repr.len();
+    uint_repr
+        .get_mut(uint_repr_len - order_repr.as_ref().len()..)
+        .expect("the curve order fits into `Uint`")
+        .copy_from_slice(order_repr.as_ref());
+    <P::Paillier as PaillierParams>::Uint::try_from_be_bytes(&uint_repr).expect("the representation size is correct")
+}
+
+fn curve_order_uint<P: SchemeParams>() -> NonZero<<P::Paillier as PaillierParams>::Uint> {
+    NonZero::new(curve_order::<P>()).expect("the curve order is non-zero")
+}
+
+fn curve_order_wide_uint<P: SchemeParams>() -> NonZero<<P::Paillier as PaillierParams>::WideUint> {
+    NonZero::new(curve_order::<P>().to_wide()).expect("the curve order is non-zero")
+}
 
 fn uint_from_scalar<P: SchemeParams>(value: &Scalar<P>) -> <P::Paillier as PaillierParams>::Uint {
     let scalar_bytes = value.to_be_bytes();
@@ -30,7 +51,7 @@ fn uint_from_scalar<P: SchemeParams>(value: &Scalar<P>) -> <P::Paillier as Paill
 pub(crate) fn public_signed_from_scalar<P: SchemeParams>(
     value: &Scalar<P>,
 ) -> PublicSigned<<P::Paillier as PaillierParams>::Uint> {
-    let order_bits = P::CURVE_ORDER.as_ref().bits_vartime();
+    let order_bits = curve_order_uint::<P>().bits_vartime();
     PublicSigned::new_positive(uint_from_scalar::<P>(value), order_bits).expect(concat![
         "a curve scalar value is smaller than the half of `PaillierParams::Uint` range, ",
         "so it is still positive when treated as a 2-complement signed value"
@@ -39,7 +60,7 @@ pub(crate) fn public_signed_from_scalar<P: SchemeParams>(
 
 /// Converts an integer to the associated curve scalar type.
 pub(crate) fn scalar_from_wide_uint<P: SchemeParams>(value: &<P::Paillier as PaillierParams>::WideUint) -> Scalar<P> {
-    let r = *value % P::CURVE_ORDER_WIDE;
+    let r = *value % curve_order_wide_uint::<P>();
 
     let repr = r.to_be_bytes();
     let uint_len = repr.as_ref().len();
@@ -81,7 +102,7 @@ pub(crate) fn scalar_from_wide_signed<P: SchemeParams>(
 fn secret_scalar_from_wide_uint<P: SchemeParams>(
     value: &Secret<<P::Paillier as PaillierParams>::WideUint>,
 ) -> Secret<Scalar<P>> {
-    let r = value % &P::CURVE_ORDER_WIDE;
+    let r = value % &curve_order_wide_uint::<P>();
 
     let repr = SecretBox::<[u8]>::from(r.expose_secret().to_be_bytes());
     let uint_len = repr.expose_secret().as_ref().len();
@@ -95,7 +116,7 @@ fn secret_scalar_from_wide_uint<P: SchemeParams>(
                 .get(uint_len - scalar_len..)
                 .expect("Uint is assumed to be bigger than Scalar"),
         )
-        .expect("the value was reduced modulo `CURVE_ORDER`, so it's a valid curve scalar")
+        .expect("the value was reduced modulo curve order, so it's a valid curve scalar")
     })
 }
 
@@ -126,7 +147,7 @@ pub(crate) fn secret_signed_from_scalar<P: SchemeParams>(
 ) -> SecretSigned<<P::Paillier as PaillierParams>::Uint> {
     SecretSigned::new_modulo(
         secret_uint_from_scalar::<P>(value),
-        &P::CURVE_ORDER,
+        &curve_order_uint::<P>(),
         <<P::Curve as CurveArithmetic>::Scalar as PrimeField>::NUM_BITS,
     )
     .expect(concat![
--- src/params/traits.rs
+++ src/params/traits.rs
@@ -4,7 +4,7 @@ use core::{fmt::Debug, ops::Add};
 // and `k256` depends on the released one.
 // So as long as that is the case, `k256` `Uint` is separate
 // from the one used throughout the crate.
-use crypto_bigint::{subtle::ConditionallySelectable, Bounded, Integer, NonZero, PowBoundedExp, RandomMod};
+use crypto_bigint::{subtle::ConditionallySelectable, Bounded, Integer, PowBoundedExp, RandomMod};
 use digest::{ExtendableOutput, Update};
 use ecdsa::hazmat::{DigestPrimitive, SignPrimitive, VerifyPrimitive};
 use elliptic_curve::{
@@ -23,27 +23,6 @@ use crate::{
     tools::hashing::Chain,
     uint::{BoxedEncoding, Extendable},
 };
-
-#[cfg(any(test, feature = "k256", feature = "dev"))]
-#[allow(clippy::indexing_slicing)]
-pub(crate) const fn upcast_uint<const N1: usize, const N2: usize>(
-    value: elliptic_curve::bigint::Uint<N1>,
-) -> elliptic_curve::bigint::Uint<N2> {
-    assert!(N2 >= N1, "Upcast target must be bigger than the upcast candidate");
-    let mut result_words = [0; N2];
-    let mut i = 0;
-    let words = value.as_words();
-    while i < N1 {
-        result_words[i] = words[i];
-        i += 1;
-    }
-    elliptic_curve::bigint::Uint::from_words(result_words)
-}
-
-#[cfg(any(test, feature = "k256", feature = "dev"))]
-pub(crate) const fn convert_uint<const N: usize>(value: elliptic_curve::bigint::Uint<N>) -> crypto_bigint::Uint<N> {
-    crypto_bigint::Uint::from_words(value.to_words())
-}
 
 /// Signing scheme parameters.
 pub trait SchemeParams: 'static + Debug + Clone + Send + PartialEq + Eq + Send + Sync + Ord + Copy + Serialize
@@ -70,10 +49,6 @@ where
 
     /// The number of bits of security provided by the scheme.
     const SECURITY_BITS: usize; // $m$ in the paper
-    /// The order of the curve.
-    const CURVE_ORDER: NonZero<<Self::Paillier as PaillierParams>::Uint>; // $q$
-    /// The order of the curve as a wide integer.
-    const CURVE_ORDER_WIDE: NonZero<<Self::Paillier as PaillierParams>::WideUint>;
     /// The scheme's statistical security parameter.
     const SECURITY_PARAMETER: usize; // $\kappa$ in the paper
     /// The bound for secret values.
@@ -127,35 +102,4 @@ where
         .chain_bytes(&P::L_BOUND.to_be_bytes())
         .chain_bytes(&P::LP_BOUND.to_be_bytes())
         .chain_bytes(&P::EPS_BOUND.to_be_bytes())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{
-        bigintv05::{U256, U64},
-        upcast_uint,
-    };
-
-    #[test]
-    fn upcast_uint_results_in_a_bigger_type() {
-        let n = U64::from_u8(10);
-        let expected = U256::from_u8(10);
-        let bigger_n: U256 = upcast_uint(n);
-
-        assert_eq!(bigger_n, expected);
-    }
-
-    #[test]
-    #[should_panic(expected = "Upcast target must be bigger than the upcast candidate")]
-    fn upcast_uint_panics_in_test_if_actually_attempting_downcast() {
-        let n256 = U256::from_u8(8);
-        let _n: U64 = upcast_uint(n256);
-    }
-
-    #[test]
-    fn upcast_uint_allows_casting_to_same_size() {
-        let n256 = U256::from_u8(8);
-        let n: U256 = upcast_uint(n256);
-        assert_eq!(n, n256)
-    }
 }
