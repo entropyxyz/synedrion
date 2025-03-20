0a1ce21a2ccb121d641ef3a52e21e019ed74c8e9
Bogdan Opanchuk <bogdan@opanchuk.net>
2025-03-20 11:27:02 -0700

    Expose `PaillierParams` and the corresponding traits.

--- src/uint.rs
+++ src/uint.rs
@@ -4,8 +4,10 @@ mod secret_signed;
 mod secret_unsigned;
 mod traits;
 
+pub use traits::{BoxedEncoding, Extendable, MulWide};
+
 pub(crate) use public_signed::PublicSigned;
 pub(crate) use public_uint::PublicUint;
 pub(crate) use secret_signed::SecretSigned;
 pub(crate) use secret_unsigned::SecretUnsigned;
-pub(crate) use traits::{BoxedEncoding, Exponentiable, Extendable, FromXofReader, IsInvertible, MulWide, ToMontgomery};
+pub(crate) use traits::{Exponentiable, FromXofReader, IsInvertible, ToMontgomery};
